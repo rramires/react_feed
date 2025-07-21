0711562034bda2cfef84f07ff1e9207b2ef902fc
rramires <ricardo@flexbr.com>
2025-07-21 10:07:25 -0300

    Understanding React Closures.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -9,7 +9,23 @@ export function Comment({ id, content, onDelete }) {
     const [clapping, setClapping] = useState(0)
 
     function handleAddApplause(){
+        /* 
         setClapping(clapping + 1)
+        // It does not get the updated value because the rendering cycle has not completed.
+        console.log('clapping: ', clapping) // clapping: 0  
+        */
+
+        /*  const actual = clapping + 1;
+        setClapping(actual)
+        //console.log('clapping: ', actual) */
+
+        setClapping((actual) => {
+            actual ++
+            console.log(actual) // Note that it renders 2x
+            // Strict mode in main.jsx (<StrictMode>) causes it to render more than once. 
+            // This doesn't happen with the build.
+            return actual
+        })
     }
 
     function handleDelete() {
